f58d202a4ab700032273785bc7340f274f727e15
sojebsikder <sojebsikder@gmail.com>
2021-02-10 20:46:10 +0600

    main

--- resources/js/components/Register.jsx
+++ resources/js/components/Register.jsx
@@ -1,82 +1,75 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import AuthService from '../services/AuthService';
 import AlertMsg from './AlertMsg';
 
-export default class Register extends Component {
-    constructor(){
-        super();
-        // Method binding
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangeEmail = this.onChangeEmail.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
 
-        // Set State
-        this.state = {
-            // User information
-            username:'',
-            email:'',
-            password:'',
-            // Error handeling variable
-            alert_message: '',
-        }
-    }
+
+export default function Register() {
+    
+    // User information
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    // Error handeling variable
+    const [alert_message, setAlert_message] = useState('');
+
 
 
     // Get value from input and set state
-    onChangeUsername(e)
+    const onChangeUsername=(e)=>
     {
-        this.setState({username:e.target.value});
+        setUsername(e.target.value);
     }
-    onChangeEmail(e)
+    const onChangeEmail=(e)=>
     {
-        this.setState({email:e.target.value});
+        setEmail(e.target.value);
     }
-    onChangePassword(e)
+    const onChangePassword=(e)=>
     {
-        this.setState({password:e.target.value});
+        setPassword(e.target.value);
     }
     // create user account
-    onSubmit(e)
+    const onSubmit=(e)=>
     {
         e.preventDefault();
         const user ={
-            name: this.state.username,
-            email: this.state.email,
-            password: this.state.password
+            name: username,
+            email: email,
+            password: password
         }
         
         AuthService.register(user, (res)=>{
-            this.setState({alert_message:'success'});
+            setAlert_message('success');
         });
     }
 
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <br />
 
-                    {this.state.alert_message=="success"?<AlertMsg type="success" msg="Account created successfully!" />:null}
-                    {this.state.alert_message=="error"?<AlertMsg type="warning" msg="Something went wrong" />:null}
+    return (
+        <div>
+            <div className="container">
+                <br />
 
-                    <form onSubmit={this.onSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="username" className="form-label">Username</label>
-                        <input type="text" onChange={this.onChangeUsername} className="form-control" id="username" placeholder="Username" autoComplete="off" />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" onChange={this.onChangeEmail} className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" autoComplete="off" />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" onChange={this.onChangePassword} className="form-control" id="password" placeholder="Password" autoComplete="off" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Sign Up</button>
-                    </form>
+                {alert_message=="success"?<AlertMsg type="success" msg="Account created successfully!" />:null}
+                {alert_message=="error"?<AlertMsg type="warning" msg="Something went wrong" />:null}
+
+                <form onSubmit={onSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Username</label>
+                    <input type="text" onChange={onChangeUsername} className="form-control" id="username" placeholder="Username" autoComplete="off" />
                 </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" onChange={onChangeEmail} className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" autoComplete="off" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" onChange={onChangePassword} className="form-control" id="password" placeholder="Password" autoComplete="off" />
+                </div>
+                <button type="submit" className="btn btn-primary">Sign Up</button>
+                </form>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+
